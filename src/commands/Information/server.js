536877e1e4eb789ef69f4ecdf235727bc12187b5
Blixer <93098699+BlixerDev@users.noreply.github.com>
2022-06-27 18:01:42 -0400

    Fix description error

--- src/commands/Information/server.js
+++ src/commands/Information/server.js
@@ -28,7 +28,7 @@ module.exports = new Command({
         }).then(data => data.json());
 
         embed.setTitle(interaction.guild.name);
-        embed.setDescription(interaction.guild.description);
+        embed.setDescription(interaction.guild.description || null);
         embed.setThumbnail(interaction.guild.iconURL({ size: 1024, dynamic: true }));
         embed.setImage(interaction.guild.bannerURL({ size: 1024 }));
         embed.addFields(
